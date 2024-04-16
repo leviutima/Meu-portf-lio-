dfa91a8b732c335c58961deec90567e7d565452e
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-04-16 17:21:41 -0300

    melhorando a responsividade do footer

--- src/Components/Rodape/Index.js
+++ src/Components/Rodape/Index.js
@@ -9,27 +9,27 @@ import { Link } from 'react-router-dom'
 export const Rodape = () => {
     return(
         <footer className={styles.rodape}>
-            <div>
-                <img className={styles.imageLevi} src={iconeLevi}></img>
-            </div>
-            <div>  
-                <span>
-                © LeviUtima™
-                </span>
-            </div>
+        <div>
+            <img className={styles.imageLevi} src={iconeLevi}></img>
+        </div>
+        <div>  
+            <span>
+            © LeviUtima™
+            </span>
+        </div>
 
-            <div className={styles.buttonContainer}>
-                <a className={styles.button} href='https://www.linkedin.com/in/levi-yuki-utima-7b3187289/'>
-                    <img className={styles.image__linkedin} src={iconeLinkedin}></img>
-                </a>
-                <a className={styles.button} href='https://github.com/leviutima'>
-                    <img className={styles.image} src={iconeGit}></img>
-                </a>
-                <a className={styles.button} href='https://www.youtube.com/channel/UCio9H7BF7xB0P_GB2HIq7QA'>
-                    <img className={styles.image__yt} src={iconeYT}></img>
-                </a>
-            </div>
-            
-        </footer>
+        <div className={styles.buttonContainer}>
+            <a className={styles.button} href='https://www.linkedin.com/in/levi-yuki-utima-7b3187289/'>
+                <img className={styles.image__linkedin} src={iconeLinkedin}></img>
+            </a>
+            <a className={styles.button} href='https://github.com/leviutima'>
+                <img className={styles.image} src={iconeGit}></img>
+            </a>
+            <a className={styles.button} href='https://www.youtube.com/channel/UCio9H7BF7xB0P_GB2HIq7QA'>
+                <img className={styles.image__yt} src={iconeYT}></img>
+            </a>
+        </div>
+        
+    </footer>
     )
 }
