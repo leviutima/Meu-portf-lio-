965cb38e79177d11c86f6328f14bf5d1912b43ee
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-04-16 15:41:04 -0300

    mudando nome de capa para cape

--- src/Components/PostCard/Index.js
+++ src/Components/PostCard/Index.js
@@ -2,14 +2,12 @@ import { Link } from 'react-router-dom'
 import styles from './Post.module.css'
 
 
-
-
 export const PostCard = ({post}) => {
     return(
         <Link to={`/post-projetos/${post.id}`}>
             <div className={styles.post}>
                 <img className={styles.capa}
-                src={`/assets/posts/${post.id}/capa.png`} alt='imagem de capa do post'/>
+                src={`/assets/posts/${post.id}/cape.png`} alt='imagem de capa do post'/>
                 <h2 className={styles.titulo}>{post.titulo}</h2>
                 <button className={styles.botaoLer}>Ler</button>
             </div>
