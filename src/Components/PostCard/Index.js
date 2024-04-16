72ce2e63cbe7831203b5047f1049e4cf517d5928
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-04-16 15:43:21 -0300

    testando

--- src/Components/PostCard/Index.js
+++ src/Components/PostCard/Index.js
@@ -7,7 +7,7 @@ export const PostCard = ({post}) => {
         <Link to={`/post-projetos/${post.id}`}>
             <div className={styles.post}>
                 <img className={styles.capa}
-                src={`/assets/posts/${post.id}/cape.png`} alt='imagem de capa do post'/>
+                src={`/assets/posts/${post.id}/capa.png`} alt='imagem de capa do post'/>
                 <h2 className={styles.titulo}>{post.titulo}</h2>
                 <button className={styles.botaoLer}>Ler</button>
             </div>
